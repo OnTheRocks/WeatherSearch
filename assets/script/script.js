7d2ebc832b458e763e15b6489218d132ce56e31c
Nathan Huber <Nathan_Huber@yahoo.com>
2019-12-06 02:59:33 -0600

    Current weather works, weather icons are working.  5day forecast is working, I just need to get them positioned better.  Still need to save search history to local storage.

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,14 +3,20 @@ console.log("Hi from Script!")
 $("#findCity").on("click", function(event) {
 
   event.preventDefault();
+  $("#liIcon").empty();
+  $("#fiveDay").empty(); 
 
   var city = $("#cityInput").val();
 
   console.log(city);
 
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=e71e90eede3cf2d964e119691ce61ab1"
+  var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&APPID=e71e90eede3cf2d964e119691ce61ab1"
+  
+
 
   console.log(queryURL);
+  console.log(fiveDayQueryURL);
   // console.log(city);
 
   $.ajax({
@@ -22,8 +28,15 @@ $("#findCity").on("click", function(event) {
         // Converts the UNIX timestamp from API pull to readable Date.
         var weatherDate = moment.unix(response.dt);
 
+        // Get weather icon.
+  var icon = response.weather[0].icon
+  var iconUrl = "http://openweathermap.org/img/wn/" + icon + ".png"
+
+        // Get lat and lon for UVI.
   var lat = response.coord.lat
-  var lon = response.coord.lat      
+  var lon = response.coord.lat   
+  
+        // create URL for UVI.
   var uvQueryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=e71e90eede3cf2d964e119691ce61ab1&lat=" + lat + "&lon=" + lon
 
   // Gets UVIndex....
@@ -31,20 +44,62 @@ $("#findCity").on("click", function(event) {
     url: uvQueryURL,
     method: "GET"
   }).then(function(uviResponse) {
-    // $("#weatherView").text(JSON.stringify(uviResponse));
 
+    // Gets 5 day
+  $.ajax({
+    url: fiveDayQueryURL,
+    method: "GET"
+  }).then(function(fiveDayResponse) { 
+
+    var icon5day = fiveDayResponse.list[0].weather[0].icon
+    var iconUrl5day = "http://openweathermap.org/img/wn/" + icon5day + ".png"
+    // $("#weatherView").text(JSON.stringify(fiveDayResponse));
+
+
+
+  //  filling single day details.
     $("#srchResult").removeClass("hidden");
-
-    $(".card-header").text(response.name + " Weather Details  --  " + weatherDate.format("MM/DD/YYYY"));    
-    $("#liTemp").text("Temperature: " +  response.main.temp);
-    $("#liHumitity").text("Humidity: " + response.main.humidity);
-    $("#liWindSpeed").text("Wind Speed: " + response.wind.speed);
+    $(".card-header").text(response.name + "  --  " + weatherDate.format("MM/DD/YYYY"));
+    $("#liTemp").text("Temperature: " +  Math.round(response.main.temp) + " °F");
+    $("#liIcon").append($("<img>").attr({"class":"icon","src":iconUrl}));
+    $("#liHiTemp").text("High: " +  Math.round(response.main.temp_max) + " °F");
+    $("#liLoTemp").text("Low: " +  Math.round(response.main.temp_min) + " °F");
+    $("#liHumitity").text("Humidity: " + response.main.humidity + "%");
+    $("#liWindSpeed").text("Wind Speed: " + response.wind.speed + " mph");
     $("#liUVI").text("UV Index: " + uviResponse.value);
-    
+
+    // 5-day forecast.
+    for (i=1; i<40; i+=8) {
+      var day = $("<div></div>");
+      day.addClass(" card mb-2 bg-primary text-white days").attr({"style":"width: 8rem"});  
+      var cardBody = $("<div></div");
+      cardBody.addClass("card-body");
+      var cardDate = moment(fiveDayResponse.list[i].dt_txt);
+      var icon5day = fiveDayResponse.list[i].weather[0].icon
+      var iconUrl5day = "http://openweathermap.org/img/wn/" + icon5day + ".png"
+      var cardIcon = $(`<div><img src="${iconUrl5day}" /></div>`); 
+      var temp5dayHigh = $("<p></p>");
+      temp5dayHigh.text("High: " + Math.round(fiveDayResponse.list[i].main.temp_max) + " °F");
+      var temp5dayLow = $("<p></p>");
+      temp5dayLow.text("Low: " + Math.round(fiveDayResponse.list[i].main.temp_min) + " °F");
+      var humidity5day = $("<p></p>");
+      humidity5day.text("Humitity: " + fiveDayResponse.list[i].main.humidity + "%");
+      cardBody.append(cardDate.format("MM/DD/YYYY"));
+      cardBody.append(cardIcon);
+      cardBody.append(temp5dayHigh);
+      cardBody.append(temp5dayLow);
+      cardBody.append(humidity5day);
+      day.append(cardBody);
+      $("#fiveDay").append(day);
+
+    };
+ 
 
 
     // debugging
 
+    console.log("The weather icon is: " + icon);
+    console.log("The weather icon URL is: " + iconUrl);
     console.log(uvQueryURL);
     console.log("Date: " +  weatherDate.format("MM/DD/YYYY"));
     console.log(response.dt);
@@ -57,7 +112,9 @@ $("#findCity").on("click", function(event) {
 
     console.log(response);
     console.log(uviResponse);
+    console.log(fiveDayResponse);
 
   });  
   }); 
+});
 });
